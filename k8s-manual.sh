2c9a74c25da845a8f25afe27bd476f53e81100dc
Hoang N.V <hoangn01@gmail.com>
2020-12-04 11:37:33 +0700

    remove uncode

--- k8s-manual.sh
+++ k8s-manual.sh
@@ -1,4 +1,4 @@
 #need to update
-docker build -t b2b/customer-service:1.0 .
-docker tag b2b/customer-service:1.0 apphub.rvbb.vn/b2b/customer-service:1.0
-docker push apphub.rvbb.vn/b2b2/customer-service:1.0
+docker build -t b2b/rsocket-server:1.0 .
+docker tag b2b/rsocket-server:1.0 apphub.rvbb.vn/b2b/rsocket-server:1.0
+docker push apphub.rvbb.vn/b2b2/rsocket-server:1.0
